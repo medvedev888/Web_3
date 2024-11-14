30b1af141d169313d9889a3968696a5b06542a6a
vladislavmedvedev <EnoT3581@mail.ru>
2024-11-14 17:37:49 +0300

    added validation.js and updated frontend;

---
+++ src/main/webapp/resources/js/validation.js
@@ -0,0 +1,11 @@
+
+// function for drawing figure on graph when selecting the R parameter
+function handleSliderChange() {
+    const r = parseFloat(document.getElementById("pointForm:rValue").value);
+
+    deleteFigures();
+    drawingFigure(r);
+    // TODO: need to change this block
+    // redrawingPoints(arrayPoints);
+
+}
