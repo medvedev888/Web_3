d17452c1296c0aacceeae5081dd50f5f646179b2
Vladislav Medvedev <EnoT3581@mail.ru>
2025-04-10 21:09:15 +0300

    Added Ant build script for lab OPI_3;

---
+++ src/main/java/me/vladislav/web_3/Main.java
@@ -0,0 +1,8 @@
+package me.vladislav.web_3;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Веб-приложение собрано. Запуск через сервер приложений.");
+    }
+}
+
