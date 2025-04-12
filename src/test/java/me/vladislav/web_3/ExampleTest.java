0d2c884dade0e22629bbf7991576accc074f7263
Vladislav Medvedev <EnoT3581@mail.ru>
2025-04-12 20:57:03 +0300

    Updated Ant build script for lab OPI_3 (added test target, team target and env target);

---
+++ src/test/java/me/vladislav/web_3/ExampleTest.java
@@ -0,0 +1,13 @@
+package me.vladislav.web_3;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void testAddition() {
+        int result = 1 + 1;
+        assertEquals(2, result, "1 + 1 должно быть равно 2");
+    }
+}
